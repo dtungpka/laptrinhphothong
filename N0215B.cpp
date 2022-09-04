4e59e88e365c7c0263097aa9973f2ec174f17839
DuongTung <duongdoanbach@gmail.com>
2022-09-04 03:04:24 -0500

    Create N0215B.cpp

---
+++ N0215B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    //input 5 long long in vector
+    vector<long long> v;
+    vector<short> counts;
+    bool flag = false;
+    //initialize vector
+    for (int i = 0; i < 5; i++) {
+        long long temp;
+        cin >> temp;
+        if (v.size() == 0) {
+            v.push_back(temp);
+            counts.push_back(1);
+        }
+        else{
+            for (int j = 0; j < v.size(); j++)if (v[j] == temp) { if (++counts[j] >= 4)flag = true; }
+            else if (v.size() - 1 == j) { v.push_back(temp); counts.push_back(1); }
+        }
+    }
+    cout << (flag ? "YES" : "NO");
+    return 0;
+
+}
